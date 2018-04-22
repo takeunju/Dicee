4546476e413a1b15b119d724c19d1b42fad695bb
Eunju Tak <teunju@gmail.com>
2018-04-22 22:56:18 +0900

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by 탁은주 on 2018. 4. 22..
+//  Copyright © 2018년 Eunju Tak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
